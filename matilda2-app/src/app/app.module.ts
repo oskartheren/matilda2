7773ba55a44db63b74557280a74dd58540ca5401
Magnus Persson <rattmuffen@gmail.com>
2019-06-12 21:03:27 +0200

    Warm welcome to the first item in the TopList!
    Remove those annoying help links, who needs those?!

--- matilda2-app/src/app/app.module.ts
+++ matilda2-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToplistService } from './toplist.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { ToplistService } from './toplist.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [ToplistService],
   bootstrap: [AppComponent]
